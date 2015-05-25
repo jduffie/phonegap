1844d941ce34919cabd99169560932f7eeee34bb
John Duffie <johnduffie91@gmail.com>
2015-05-24 22:28:22 -0400

    single page app

--- js/main.js
+++ js/main.js
@@ -14,13 +14,14 @@ var app = {
     },
 
     initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        initialize: function() {
+            var self = this;
+            this.store = new MemoryStore(function() {
+                self.renderHomeView();
+            });
+        }
     },
-    
+
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -28,6 +29,17 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
+
+    renderHomeView: function() {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
 
 };
 
